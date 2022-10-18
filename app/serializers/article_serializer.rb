580f93aeda921f4e7ea796e3cffebdd54b41259b
vietle.nk <vietle.nk@gmail.com>
2022-10-18 14:00:00 +0700

    Add explain structure of article model in serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  slug       :string
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :slug
 end
